cdf8b2091756f61763e1c174cdb0c38b54880da3
kanamiinaba <a5816012@aoyama.jp>
2018-07-09 17:56:06 +0900

    Modified templates to display posts from database.

--- mysite/urls.py
+++ mysite/urls.py
@@ -16,10 +16,8 @@ Including another URLconf
 from django.conf.urls import include
 from django.conf.urls import url
 from django.contrib import admin
-from django.conf import settings
-from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
 	url(r'', include('blog.urls')),
-]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
